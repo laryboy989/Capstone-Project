de0af894a591d8a372efd6aa17f47cfce414c847
maskerbai <jintan-bai@uiowa.edu>
2014-04-03 12:10:33 -0500

    changed menu and add webconfig to each role

--- 6K185_Big/Site.master.vb
+++ 6K185_Big/Site.master.vb
@@ -2,8 +2,5 @@
 Partial Class Site
     Inherits System.Web.UI.MasterPage
 
-    Protected Sub NavigationMenu_MenuItemClick(sender As Object, e As MenuEventArgs) Handles NavigationMenu.MenuItemClick
-
-    End Sub
 End Class
 
